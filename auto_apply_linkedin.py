3d21d426e76b21bf4245d8ee2634ead08089f5dd
Chris Lo <clo512atx@gmail.com>
2023-03-16 15:03:24 -0500

    code reformatted, time.sleep erased, added WebDriverWait()

--- auto_apply_linkedin.py
+++ auto_apply_linkedin.py
@@ -1,4 +1,4 @@
-#BH RN auto job apply w/ linkedin easy apply
+# BH RN auto job apply w/ linkedin easy apply
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
@@ -9,10 +9,7 @@ from selenium.common.exceptions import TimeoutException, NoSuchElementException,
 import json
 import time
 
-
-
 class ApplyLinkedin:
-
     def __init__(self,data) -> None:
         self.email = data['email']
         self.password = data['password']
@@ -22,47 +19,33 @@ class ApplyLinkedin:
         
 
     def login(self):
-        
         self.driver.get('https://www.linkedin.com/login')
         self.driver.maximize_window()
         
-        
-#enter in our email/password and submit
-        login_email = self.driver.find_element('name', 'session_key')
+        # enter in our email/password and submit
+        login_email = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.NAME, "session_key")))
         login_email.send_keys(self.email)
-        time.sleep(2)
 
-        password = self.driver.find_element('name', 'session_password')
+        password = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.NAME, "session_password")))
         password.send_keys(self.password)
-        time.sleep(2)
         
         password.send_keys(Keys.RETURN)
-        time.sleep(20)
 
     def jobSearch(self):
-#keyword and location on jobs tab
-        jobs_link = self.driver.find_element('link text','Jobs')
+        # keyword and location on jobs tab
+        jobs_link = WebDriverWait(self.driver, 30).until(EC.visibility_of_element_located((By.LINK_TEXT, "Jobs")))
         jobs_link.click()
-        time.sleep(2)
         
-#insert keyword and location
-        keyword_search = self.driver.find_element('xpath','/html/body/div[5]/header/div/div/div/div[2]/div[2]/div/div/input[1]')
+        # insert keyword and location
+        keyword_search = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[5]/header/div/div/div/div[2]/div[2]/div/div/input[1]')))
         keyword_search.clear()
         keyword_search.send_keys(self.keywords)
-        time.sleep(2)
 
-        location_search = self.driver.find_element('xpath','/html/body/div[5]/header/div/div/div/div[2]/div[3]/div/div/input[1]')
+        location_search = WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[5]/header/div/div/div/div[2]/div[3]/div/div/input[1]')))
         location_search.clear()
-        location_search.send_keys(self.location)
-        time.sleep(2)
-        
+        location_search.send_keys(self.location)        
         location_search.send_keys(Keys.RETURN)
-        time.sleep(2)
-
-#filter selections/criterias (PRN,part time, other, and fulltime jobs.) 
-#Add beh hosp. to network (Georgetown, Acadia, HCS, Rock springs, Ascension)
-#Most relevant, and job type=Nurse
-
+        
     def exit_application(self):
         cancel_button = self.driver.find_element(By.XPATH, '//li-icon[contains(@type, "cancel-icon")]')
         cancel_button.click()
@@ -80,9 +63,8 @@ class ApplyLinkedin:
             print("application already exited, moving to the next one")
 
     
-    
     def easyApply(self):
-        easy_apply_button = self.driver.find_element('xpath','/html/body/div[5]/div[3]/div[4]/section/div/section/div/div/div/ul/li[9]/div/button')
+        easy_apply_button = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.XPATH, '/html/body/div[5]/div[3]/div[4]/section/div/section/div/div/div/ul/li[9]/div/button')))
         easy_apply_button.click()
         time.sleep(5)
             
@@ -140,14 +122,9 @@ class ApplyLinkedin:
 
                 # years of experience
                 try:
-                    # years_of_exp_labels = self.driver.find_elements(By.XPATH, '//label[contains(@for, "single-line-text-form-component")]')
-                    # for years_of_exp in years_of_exp_labels:
-                    #     prompt = years_of_exp.get_attribute('innerHTML').lower()
-                    #     if "years" in prompt:
-                    #         years_of_exp_input = prompt.find_element(By.XPATH, '//input[contains(@id, "single-line-text-form-component")]')
-                    #         years_of_exp_input.send_keys("6")
                     years_of_exp_labels = self.driver.find_elements(By.XPATH, '//input[contains(@for, "single-line-text-form-component")]')
                     for years_of_exp in years_of_exp_labels:
+                        years_of_exp.clear()
                         years_of_exp.send_keys("6")
                 except:
                     print("no years of experience prompts")
@@ -178,65 +155,13 @@ class ApplyLinkedin:
             self.finish_submission()
             time.sleep(3)
 
-        # allfilter_button = self.driver.find_element('xpath','/html/body/div[5]/div[3]/div[4]/section/div/section/div/div/div/div/div/button')
-        # allfilter_button.click()
-        # time.sleep(2)
-    
-
-        # most_relevant = self.driver.find_element('xpath','/html/body/div[3]/div/div/div[2]/ul/li[2]/fieldset/div/ul/li[2]/input')
-        # most_relevant.click()
-        # time.sleep(2)
-
-        # anytime_button = self.driver.find_element('xpath','/html/body/div[3]/div/div/div[2]/ul/li[3]/fieldset/div/ul/li[1]/input')
-        # anytime_button.click()
-        # time.sleep(5)
-
-        # fulltime_button = self.driver.find_element('xpath','/html/body/div[3]/div/div/div[2]/ul/li[6]/fieldset/div/ul/li[1]/input')
-        # fulltime_button.click()
-        # time.sleep(5)
-        # parttime_button = self.driver.find_element('xpath','/html/body/div[3]/div/div/div[2]/ul/li[6]/fieldset/div/ul/li[2]/input')
-        # parttime_button.click()
-        # time.sleep(5)
-        # contract_button = self.driver.find_element('xpath','/html/body/div[3]/div/div/div[2]/ul/li[6]/fieldset/div/ul/li[3]/input')
-        # contract_button.click()
-        # time.sleep(5)
-        # other_button = self.driver.find_element('xpath','/html/body/div[3]/div/div/div[2]/ul/li[6]/fieldset/div/ul/li[4]/input')
-        # other_button.click()
-        # time.sleep(5)
-
-        # show_results = self.driver.find_element('xpath','/html/body/div[3]/div/div/div[3]/div/button[2]')
-        # show_results.click()
-        # time.sleep(2)
-
-
-
-'''
-#adding in hospitals that are not on general search field
-        add_companies = self.driver.find_element('xpath','/html/body/div[3]/div/div/div[2]/ul/li[5]/fieldset/div/ul/li[19]/button/span')
-        add_companies.click()
-        time.sleep(5)
-
-        add_companies.send_keys(self.companyone)
-        rocksprings = self.driver.find_element('xpath','/html/body/div[3]/div/div/div[2]/ul/li[5]/fieldset/div/ul/li[19]')
-        rocksprings.click()
-'''        
-
-
-
-        
-
-
 
 if __name__ =="__main__":
-
     with open(r'data.json') as user_data:
         data = json.load(user_data)
     bot = ApplyLinkedin(data)
     bot.login()
-    time.sleep(5)
     bot.jobSearch()
-    time.sleep(5)
     bot.easyApply()
     time.sleep(5)
 
-    
